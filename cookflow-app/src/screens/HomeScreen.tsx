ce1cf41815488cfaafb041de290b7cee28dbdcfd
Fernando Silva <fernando241204@gmail.com>
2025-04-19 20:49:26 -0300

    ContinuandoApp

--- cookflow-app/src/screens/HomeScreen.tsx
+++ cookflow-app/src/screens/HomeScreen.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
+import { ScrollView } from 'react-native-reanimated/lib/typescript/Animated';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { UserIcon, BellIcon, MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import Categories from '../components/categories';
 
 export default function HomeScreen() {
+
+  const [activeCategory, setActiveCategory] = useState('Beef');
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View className='flex-1 bg-white'>
+      <StatusBar style='dark' />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 50}}
+        className={'space-y-6 pt-14'}
+      >
+        {/* Avatar e icone do sino */}
+        <View className='mx-4 flex-row justify-between items-center mb-2'>
+          <UserIcon size={hp(4)} color='gray' />
+          <BellIcon size={hp(4)} color='gray' />
+        </View>
+
+        {/* Agradecimentos e frase de efeito legal */}
+        <View className='mx-4 space-y-2 mb-2'>
+          <Text style={{fontSize: hp(1.7)}} className='text-neutral-600'>Olá, Fer!</Text>
+          <View>
+            <Text style={{fontSize: hp(3.8)}} className='font-semibold text-neutral-600'>Faça sua refeição de forma divertida,</Text>
+          </View>
+          <Text style={{fontSize: hp(3.8)}}>
+            Sem sair de <Text className='text-amber-400'>casa</Text>
+          </Text>
+        </View>
+            
+        {/* Barra de pesquisa */}
+        <View className='mx-4 flex-row items-center rounded-full bg-black/5 p-[6px]'>
+          <TextInput 
+            placeholder='Pesquise uma receita'
+            placeholderTextColor={'gray'}
+            style={{fontSize: hp(1.7)}}
+            className='flex-1 text-base mb-1 pl-3 tracking-wider'
+          />
+          <View className='bg-white rounded-full p-3'>
+            <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color='gray' />
+          </View>
+        </View>
+
+        {/* Categorias */}
+        <View>
+          <Categories activeCategory={activeCategory} setActiveCategory={setActiveCategory}/>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- cookflow-app/src/screens/WelcomeScreen.tsx
+++ cookflow-app/src/screens/WelcomeScreen.tsx
@@ -1,18 +1,47 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation } from 'expo-router';
 
 
 export default function WelcomeScreen() {
+
+  const ring1padding = useSharedValue(0);
+  const ring2padding = useSharedValue(0);
+
+  const navigation = useNavigation()
+
+  useEffect(()=> {
+        ring1padding.value = 0;
+        ring2padding.value = 0;
+        setTimeout(() => ring1padding.value = withSpring(ring1padding.value+hp(5)), 100);
+        setTimeout(() => ring2padding.value = withSpring(ring2padding.value+hp(5.5)), 300);
+
+       //setTimeout(() => navigation.navigate('Home'), 2500)
+  }, [])
+
   return (
     <View className='flex-1 items-center justify-center space-y-10 bg-amber-500'>
         <StatusBar style="light" />
 
-        <View className='bg-white/20 rounded-full'>
-            <View className='bg-white/20 rounded-full'>
-                
-            </View>
+        {/* Logo com anéis */}
+        <Animated.View className='bg-white/20 rounded-full' style={{padding: ring2padding}}>
+            <Animated.View className='bg-white/20 rounded-full' style={{padding: ring1padding}}>
+                {/* Aqui vai a imagem, mas ainda não sei como colocar :/ */}
+            </Animated.View>
+        </Animated.View>
+
+        {/* Título */}
+        <View className='flex items-center space-y-2'>
+          <Text style={{fontSize: hp (7)}} className='font-bold text-white tracking-widest'>
+              Cookflow
+          </Text>
+          <Text style={{fontSize: hp(2)}} className='font-medium text-white tracking-widest'>
+              Cozinhando de forma divertida, ou qualquer outra frase de efeito.
+          </Text>
         </View>
     </View>
-  );
+  )
 }
