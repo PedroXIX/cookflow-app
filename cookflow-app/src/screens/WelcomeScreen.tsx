61a55a32f14a2b0be1d90c4ca4c89f046e8935ee
Fernando Silva <fernando241204@gmail.com>
2025-04-19 13:19:29 -0300

    CommitConfiguracoes

--- cookflow-app/src/screens/WelcomeScreen.tsx
+++ cookflow-app/src/screens/WelcomeScreen.tsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
+
 export default function WelcomeScreen() {
   return (
     <View className='flex-1 items-center justify-center space-y-10 bg-amber-500'>
